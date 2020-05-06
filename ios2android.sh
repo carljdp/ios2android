beff88339f927004f7172e59a45beb416ee8345d
carljdp <carljdp@localhost.localdomain>
2020-05-06 23:17:18 +0200

    analyse & comment working of script

--- ios2android.sh
+++ ios2android.sh
@@ -1,46 +1,31 @@
 #!/bin/bash
 
-# ios2android - a simple script to port iOS retina images to
-#  the appropriate Android density buckets
+# F U N C S
 
-# For more information: 
-#  https://github.com/Ninjanetic/ios2android
-
-# Requires Imagemagick
-#  To install on Mac: brew install imagemagick
-
-# Floating point number functions from: 
-#  http://www.linuxjournal.com/content/floating-point-math-bash
-
-############################################################
-# Default scale used by float functions.
-
-float_scale=2
-
-
-############################################################
 # Evaluate a floating point number expression.
+float_eval () {
+    local decimalPrecision=2
+    local calcExitCode=0
+    local calcResult=0.0
 
-function float_eval()
-{
-    local stat=0
-    local result=0.0
-    if [[ $# -gt 0 ]]
+    if [[ $# -gt 0 ]] # count of args > 0 ?
     then
-        result=$(echo "scale=$float_scale; $*" | bc -q 2>/dev/null)
-        stat=$?
-        if [[ $stat -eq 0  &&  -z "$result" ]]
+        calcResult=$(echo "scale=$decimalPrecision; $*" | bc -q 2>/dev/null)
+        calcExitCode=$? # exit code of preceding statement
+        if [[ $stat -eq 0  &&  -z "$calcResult" ]]
         then
             stat=1
         fi
     fi
-    echo $result
-    return $stat
+    echo $calcResult
+    return $calcExitCode
 }
 
-############################################################
-# Process images.
+# M A I N 
 
+echo -e "\n[ios2android] --- START ---"
+
+echo "[ios2android] remove output directories"
 rm -rf drawable-ldpi
 rm -rf drawable-mdpi
 rm -rf drawable-hdpi
@@ -50,6 +35,7 @@ rm -rf drawable-large-ldpi
 rm -rf drawable-large-mdpi
 rm -rf drawable-large-hdpi
 rm -rf drawable-large-xhdpi
+echo "[ios2android] re-create output directories"
 mkdir drawable-ldpi
 mkdir drawable-mdpi
 mkdir drawable-hdpi
@@ -60,27 +46,29 @@ mkdir drawable-large-mdpi
 mkdir drawable-large-hdpi
 mkdir drawable-large-xhdpi
 
-for existingFile in *.jpg *.png; do 
-    if [[ -f $existingFile ]]
+for existingFile in *.jpg *.png  # any file or dir match
+do 
+    if [[ -f $existingFile ]]  # exists and is a regular file
     then
-        if [[ ${existingFile} =~ ~ipad ]]
+        echo "[ios2android] processing $existingFile"
+        if [[ ${existingFile} =~ ~ipad ]]  # case-insensitive match
         then
-            newFile=${existingFile/@2x~ipad./.}
-            newFile=${newFile/-/_}
+            newFile=${existingFile/@2x~ipad./.}  # remove @2x~ipad
 
-            if [[ ${newFile} =~ ^[0-9] ]]
+            newFile=${newFile/-/_}  # replace - with _
+            if [[ ${newFile} =~ ^[0-9] ]]  # regex starts with digit
             then
-                newFile="img_${newFile}"
+                newFile="img_${newFile}"  # prefix with img_
             fi
 
-            newFile=`echo $newFile | tr "[:upper:]" "[:lower:]"`
+            newFile=`echo $newFile | tr "[:upper:]" "[:lower:]"`  # to lower-case
             echo $existingFile 
             convert -resize 37.5% $existingFile drawable-large-ldpi/$newFile
             convert -resize 50% $existingFile drawable-large-mdpi/$newFile
             convert -resize 75% $existingFile drawable-large-hdpi/$newFile
             convert -resize 112.5% $existingFile drawable-large-xhdpi/$newFile 
 
-            if [[ $# -gt "0" ]]
+            if [[ $# -gt "0" ]]  # count of args > 0 ?
             then
                 # if a parameter is specified, use it as a 
                 #  pre-scaling value to create images targeted 
@@ -96,47 +84,50 @@ for existingFile in *.jpg *.png; do
                 convert -resize $xhdpi% $existingFile drawable-xhdpi/$newFile 
                 convert -resize $xxhdpi% $existingFile drawable-xxhdpi/$newFile 
             fi
-        elif [[ ${existingFile} =~ @3x ]]
+        elif [[ ${existingFile} =~ @3x ]]  # case-sensitive match
         then
-            newFile=${existingFile/@3x./.}
-            if [[ ${newFile} =~ -568h ]]
+            newFile=${existingFile/@3x./.}  # remove @3x
+
+            if [[ ${newFile} =~ -568h ]]  # contains
             then
-                newFile=${newFile/-568h./.}
+                newFile=${newFile/-568h./.}  # remove -568h
             fi
 
-            newFile=${newFile//-/_}
-
-            if [[ ${newFile} =~ ^[0-9] ]]
+            newFile=${newFile//-/_}  # replace - with _
+            if [[ ${newFile} =~ ^[0-9] ]]  # starts with digit
             then
-                newFile="img_${newFile}"
+                newFile="img_${newFile}"  # prefix with img_
             fi
 
-            newFile=`echo $newFile | tr "[:upper:]" "[:lower:]"`
+            newFile=`echo $newFile | tr "[:upper:]" "[:lower:]"`  # to lower-case
             echo $existingFile
             convert -resize 25% $existingFile drawable-ldpi/$newFile
             convert -resize 33% $existingFile drawable-mdpi/$newFile
             convert -resize 50% $existingFile drawable-hdpi/$newFile
             convert -resize 66.6% $existingFile drawable-xhdpi/$newFile
-            cp $existingFile drawable-xxhdpi/$newFile
+            cp $existingFile drawable-xxhdpi/$newFile  # copy original
         else
-            newFile=${existingFile/@2x./.}
-            if [[ ${newFile} =~ -568h ]]
-            then
-                newFile=${newFile/-568h./.}
-            fi
-            newFile=${newFile//-/_}
+            newFile=${existingFile/@2x./.}  # remove @2x
 
-            if [[ ${newFile} =~ ^[0-9] ]]
+            if [[ ${newFile} =~ -568h ]]  # contains
             then
-                newFile="img_${newFile}"
+                newFile=${newFile/-568h./.}  # remove -568h
             fi
 
-            newFile=`echo $newFile | tr "[:upper:]" "[:lower:]"`
+            newFile=${newFile//-/_}  # replace - with _
+            if [[ ${newFile} =~ ^[0-9] ]]  # starts with digit
+            then
+                newFile="img_${newFile}"  # prefix with img_
+            fi
+
+            newFile=`echo $newFile | tr "[:upper:]" "[:lower:]"`  # to lower-case
             echo $existingFile
             convert -resize 37.5% $existingFile drawable-ldpi/$newFile
             convert -resize 50% $existingFile drawable-mdpi/$newFile
             convert -resize 75% $existingFile drawable-hdpi/$newFile
-            cp $existingFile drawable-xhdpi/$newFile 
+            cp $existingFile drawable-xhdpi/$newFile  # copy original
         fi
     fi
 done
+
+echo -e "[ios2android] --- END ---\n"
